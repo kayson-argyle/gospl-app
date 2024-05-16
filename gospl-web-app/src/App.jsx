2d40980b24785903296532ec341439defe4e680e
Kayson <a02303285@usu.edu>
2024-05-16 12:01:29 -0600

    inital commit

---
+++ gospl-web-app/src/App.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Delete from "./pages/delete";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="delete" element={<Delete />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />);
---
+++ gospl-web-app/src/pages/delete.jsx
@@ -0,0 +1,30 @@
+const Delete = () => {
+    return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "100px"}}>
+        <h2>Account Deletion Instructions</h2>
+        We’re sad to see you go- follow the instructions below to delete your account.
+<br />
+<br />
+To delete your account (email and password) from our servers, follow these steps:  
+<br />
+<br />
+1. Navigate to settings within the Gospl App
+<br />
+<br />
+2. Tap “Delete Account” next to your name
+<br />
+<br />
+3. You will be required to enter your account email and password
+<br />
+<br />
+4. Confirm the deletion and wait for the Login screen to appear
+<br />
+<br />
+5. Your account is now deleted
+<br />
+<br />
+Note: Notes and highlights made in collaborative collections will not be deleted, so think before you mark!
+    </div>);
+  };
+  
+  export default Delete;
